dde6f38617b46e945ed2ee70906d2c1e13149f33
devmarufurrahman <mdmarufur5682@gmail.com>
2025-04-22 08:57:39 +0600

    api service added get product list and search product

---
+++ lib/core/network/api_service.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:e_commerce_listing_app/core/config/base_url.dart';
+
+class ApiService {
+  final Dio dio = Dio(BaseOptions(baseUrl: baseUrl));
+
+  // Fetch all products from the API with pagination
+  Future<Response> getProducts({int? limit, int? skip}) async {
+    try{
+      return await dio.get('/products', queryParameters: {
+        'limit': limit,
+        'skip': skip,
+      });
+    } catch(e){
+      rethrow;
+    }
+  }
+
+  Future<Response> searchProducts(String query) async {
+    try{
+      return await dio.get('/products/search', queryParameters: {
+        'q': query,
+      });
+    } catch(e){
+      rethrow;
+    }
+  }
+
+}
