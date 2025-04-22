11bb336c5c4bfd10786c47a3a20fe36e5fd2c5ac
devmarufurrahman <mdmarufur5682@gmail.com>
2025-04-22 14:50:51 +0600

    provider update for pagination

--- lib/features/product/presentation/provider/product_provider.dart
+++ lib/features/product/presentation/provider/product_provider.dart
@@ -3,11 +3,29 @@ import '../../../../core/network/api_service.dart';
 import '../../data/models/product_model.dart';
 import '../../data/repository/product_repository.dart';
 
-final apiServiceProvider = Provider((ref) => ApiService());
+// api service call
+final apiServiceProvider = Provider<ApiService>((ref) => ApiService());
 
-final productRepositoryProvider = Provider(
-    (ref) => ProductRepository(ref.read(apiServiceProvider))
-);
+final productRepositoryProvider = Provider<ProductRepository>(
+    (ref) => ProductRepository(ref.read(apiServiceProvider)));
+
+// product list provider
+final productListProvider =
+    FutureProvider.family<List<Product>, int>((ref, page) async {
+  final repository = ref.read(productRepositoryProvider);
+
+  final response = await repository.getProducts(
+    limit: 10,
+    skip: page * 10,
+  );
+
+  return response.products;
+});
 
 
-
+ // search provider
+final productSearchProvider =
+FutureProvider.family<List<Product>, String>((ref, query) async {
+  final repository = ref.read(productRepositoryProvider);
+  return repository.searchProducts(query);
+});
